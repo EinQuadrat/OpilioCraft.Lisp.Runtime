f84504aa902acc915dd94032b8e125b853d82f2b
Carsten Schäfer <carsten.schaefer@mailbox.org>
2022-04-18 22:20:45 +0200

    Enhanced "contains" and "matches" to allow non-string values.

--- Functions.fs
+++ Functions.fs
@@ -5,14 +5,14 @@ open System.Text.RegularExpressions
 open OpilioCraft.FSharp.Prelude
 open OpilioCraft.Lisp
 
-let bfuncContains (exprA, exprB) : Expression =
+let private applyFunction funcName funcImpl exprA exprB : Expression =
     match (exprA, exprB) with
-    | Atom (FlexibleValue.String value) , Atom (FlexibleValue.String pattern) ->
-        value.Contains(pattern) |> LispBoolean
-    | _ -> raise <| InvalidLispExpressionException $"contains expects two strings arguments"
+    | Atom (FlexibleValue.String value) , Atom (FlexibleValue.String pattern) -> funcImpl value pattern |> LispBoolean
+    | _ , Atom (FlexibleValue.String pattern) -> LispFalse
+    | _ -> raise <| InvalidLispExpressionException $"{funcName} expects a string pattern as second argument"
+
+let bfuncContains (exprA, exprB) : Expression =
+    applyFunction "contains" (fun value pattern -> value.Contains(pattern)) exprA exprB
 
 let bfuncMatches (exprA, exprB) : Expression =
-    match (exprA, exprB) with
-    | Atom (FlexibleValue.String value) , Atom (FlexibleValue.String pattern) ->
-        Regex.IsMatch(value, pattern) |> LispBoolean
-    | _ -> raise <| InvalidLispExpressionException $"matches expects two string arguments"
+    applyFunction "matches" (fun value pattern -> Regex.IsMatch(value, pattern)) exprA exprB
